e26c222d53f1c018f2f189834f4b9754994583bc
Riley Kuhlman <rileyk42@icloud.com>
2018-11-22 15:09:20 -0600

    Added backstory

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class Title : MonoBehaviour {
 
+    public Animator backStory;
+
+    private int clicks = 0;
+
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (clicks < 2)
         {
-            GetComponent<Animator>().SetTrigger("FloatAway");
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (clicks == 0)
+                {
+                    GetComponent<Animator>().SetTrigger("FloatAway");
+                    backStory.SetTrigger("FloatUp");
+                } else if (clicks == 1)
+                {
+                    backStory.SetTrigger("FloatAway");
+                }
+                clicks++;
+            }
         }
     }
 }
